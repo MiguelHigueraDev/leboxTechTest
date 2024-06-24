9a13fa45cd94f10c03d365a4c349b738c06b02fd
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-24 16:06:07 -0400

    add AuthController to login and generate JWT to send to the client

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+// Login route
+Route::post('/login', [AuthController::class, 'login']);
 
 // User CRUD routes
 Route::get('/users', [UserController::class, 'index']);
