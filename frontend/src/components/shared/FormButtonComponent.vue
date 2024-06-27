3b62a9ed80179e91f30fe057d712f1afc7acfdc2
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-26 20:15:44 -0400

    add FormButton and FormInput components

---
+++ frontend/src/components/shared/FormButtonComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    @click="submitForm"
+    class="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+  >
+    Iniciar sesión
+  </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['submit'])
+
+const submitForm = () => {
+  emit('submit')
+}
+</script>
