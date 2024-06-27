bc86a6d711a67eda8a21d8c35089cb5c4f1ef16a
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-27 17:15:33 -0400

    add LogoutButtonComponent

---
+++ frontend/src/components/shared/LogoutButtonComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+    @click="logout"
+  >
+    Cerrar sesión
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { useNotificationStore } from '@/stores/notifications'
+
+const authStore = useAuthStore()
+const notifications = useNotificationStore()
+
+const logout = () => {
+  authStore.logout()
+  notifications.addNotification('Cierre de sesión exitoso.', 'success')
+}
+</script>
