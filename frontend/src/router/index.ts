2d51e2ca6923de9b6ad127b37a8d5dfe050725c4
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-26 22:28:24 -0400

    add dummy DashboardView to redirect the user after logging in

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,25 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import { useAuthStore } from '@/stores/auth'
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
+      component: DashboardView
+    },
+    {
+      path: '/login',
+      name: 'login',
       component: LoginView
     }
-    /*
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-      */
   ]
+})
+
+// Redirect to login page if not logged in and trying to access a restricted page
+router.beforeEach(async (to) => {
+  const publicRoutes = ['/login']
+  const authRequired = !publicRoutes.includes(to.path)
+  const auth = useAuthStore()
+
+  if (authRequired && !auth.user.token) {
+    auth.returnUrl = to.fullPath
+    return { name: 'login' }
+  }
 })
 
 export default router
---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main style="height: calc(100% - 44)">
+    <h1>hello world!</h1>
+  </main>
+</template>
+
+<script setup></script>
