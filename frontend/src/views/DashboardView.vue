d6fe828f7655e32313656a9d9acaeab211bf8721
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-27 17:17:21 -0400

    add dummy DashboardView as a placeholder for the logged in view

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,7 +1,19 @@
 <template>
   <main style="height: calc(100% - 44)">
-    <h1>hello world!</h1>
+    <div
+      class="mt-3 mx-auto max-w-4xl bg-white rounded-lg xl:p-0 flex flex-col items-center justify-center"
+    >
+      <div class="flex flex-col md:flex-row justify-between w-full items-center">
+        <h1 class="flex-grow text-2xl font-bold">Sistema de Gestión de Usuarios</h1>
+        <LogoutButtonComponent />
+      </div>
+    </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import LogoutButtonComponent from '@/components/shared/LogoutButtonComponent.vue'
+import { useNotificationStore } from '@/stores/notifications'
+
+const notifications = useNotificationStore()
+</script>
