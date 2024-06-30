f154bd1e92ed1ab8962aac41596f7b2c582e0dd0
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-29 23:57:08 -0400

    add close class to base modal x button to differenciate

--- frontend/src/components/modals/BaseModal.vue
+++ frontend/src/components/modals/BaseModal.vue
@@ -8,7 +8,7 @@
                         {{ props.title }}
                     </h3>
                     <button type="button" @click="closeModal"
-                        class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                        class="close end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                         data-modal-hide="authentication-modal">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 14 14">
