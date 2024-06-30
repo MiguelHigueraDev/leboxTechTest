c3dae892887d517cb035de449b5a639b3b8ed934
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-29 23:56:03 -0400

    add delete confirmation modal

--- frontend/src/components/shared/FormButtonComponent.vue
+++ frontend/src/components/shared/FormButtonComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <button type="button" :disabled="isLoading || isDisabled"
-    class="w-full flex items-center justify-center gap-4 text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+    class="form-button w-full flex items-center justify-center gap-4 text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
     :class="{
       'cursor-not-allowed opacity-50': isLoading || isDisabled,
       success: type === 'success',
