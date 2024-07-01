5f1ccf59ccaaed72355cc3a5c0cd70f552a0cb6b
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-01 19:08:50 -0400

    add AnimatedLoader to create loading skeletons

---
+++ frontend/src/components/shared/skeletons/AnimatedLoader.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="skeleton"></div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    height?: string
+    width?: string
+    borderRadius?: string
+}>()
+</script>
+
+<style scoped>
+.skeleton {
+  height: v-bind(height);
+  width: v-bind(width);
+  border-radius: v-bind(borderRadius);
+  background-image: linear-gradient(
+      to right,
+      #d6d7d8 0%,
+      #e2e3e4 10%,
+      #d6d7d8 20%,
+      #d6d7d8 100%
+  );
+  background-size: 200% 100%;
+  animation-name: loading;
+  animation-duration: 1.5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+@keyframes loading {
+  0% {
+      background-position: 50% 0;
+  }
+  100% {
+      background-position: -150% 0;
+  }
+}
+</style>
