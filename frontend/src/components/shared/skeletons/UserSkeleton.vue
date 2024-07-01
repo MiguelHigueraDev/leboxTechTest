32eb0d42a80d4074629f347e9a14c2985c13f980
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-01 19:26:24 -0400

    remove unused div

--- frontend/src/components/shared/skeletons/UserSkeleton.vue
+++ frontend/src/components/shared/skeletons/UserSkeleton.vue
@@ -7,8 +7,8 @@
             <AnimatedLoader width="180px" height="20px" />
         </td>
         <td class="px-6 py-[18px] font-medium text-gray-900 whitespace-nowrap flex gap-3">
-            <div><AnimatedLoader width="50px" height="32px" borderRadius="4px" /></div>
-            <div><AnimatedLoader width="70px" height="32px" borderRadius="4px" /></div>
+            <AnimatedLoader width="50px" height="32px" borderRadius="4px" />
+            <AnimatedLoader width="70px" height="32px" borderRadius="4px" />
         </td>
     </tr>
 </template>
