ed2814573615e99b400e488752e1f2a25114a32f
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-01 14:03:09 -0400

    improve form handling, allowing for enter to submit it

--- frontend/src/components/shared/FormButtonComponent.vue
+++ frontend/src/components/shared/FormButtonComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :disabled="isLoading || isDisabled"
+  <button type="submit" :disabled="isLoading || isDisabled"
     class="form-button w-full flex items-center justify-center gap-4 text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
     :class="{
       'cursor-not-allowed opacity-50': isLoading || isDisabled,
