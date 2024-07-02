24e5fa56bd8b983e6afbb1465110a5334ad70aa1
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-02 19:38:38 -0400

    fix transparent notifications

--- frontend/src/components/shared/NotificationComponent.vue
+++ frontend/src/components/shared/NotificationComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <li
-    class="p-4 rounded-md border-l-8 shadow-lg drop-shadow-lg'"
+    class="p-4 rounded-md border-l-8 shadow-lg drop-shadow-lg bg-white"
     :class="notification.type === 'success' ? 'border-green-400' : 'border-red-400'"
   >
     {{ notification.message }}
